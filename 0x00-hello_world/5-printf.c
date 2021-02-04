5debc0210e9cc47850d7a298301fd99d81ebabe6
darbumas <2314@holbertonschool.com>
2021-02-03 23:19:58 -0600

    the other half

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    char quote[] = {"with proper grammar, but the outcome is a piece of art,"};
+
+    printf("%s\n", quote);
+    return (0);
+}
