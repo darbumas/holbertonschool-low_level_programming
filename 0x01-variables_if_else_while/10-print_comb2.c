4979c23fa7a37a5456d59dfb1e02ffa57e139212
darbumas <2314@holbertonschool.com>
2021-02-05 06:47:16 +0000

    last ones for the night

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -5,4 +5,21 @@
 *
 * Return: Always 0
 */
+int main(void)
+{
+	int i = 0;
 
+	while (i < 100)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		if (i < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
