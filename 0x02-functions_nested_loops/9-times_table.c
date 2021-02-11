3884e611a1894cd3ae4a6243fc9060d6b617dbcc
darbumas <2314@holbertonschool.com>
2021-02-11 02:31:32 +0000

    previous syntax only compatible in c99

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,17 +1,19 @@
 #include "holberton.h"
 
 /**
- * times_table - ƒ() prints the 9 times table, from 0
+ * times_table - ƒ() prs the 9 times table, from 0
  *
  * Return: Always 0
  */
 void times_table(void)
 {
-	for (int x = 0; x <= 9; x++)
+	int x, y, mult;
+
+	for (x = 0; x <= 9; x++)
 	{
-		for (int y = 0; y <= 9; y++)
+		for (y = 0; y <= 9; y++)
 		{
-			int mult = (x * y);
+			 mult = (x * y);
 
 			if (y == 0)
 			_putchar(mult + '0');
