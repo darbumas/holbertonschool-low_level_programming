30bf526ef4a33117bbc7dfa31ddd7cddfed71fea
darbumas <2314@holbertonschool.com>
2021-02-17 13:48:40 -0600

    add ƒ() definition for _strlen()

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -21,6 +21,13 @@ void rev_string(char *s)
 		str_beg++;
 	}
 }
+
+/**
+ * _strlen - ƒ() returns the length of a string.
+ * @s: pointer to a char (the string in question).
+ *
+ * Return: Length of string
+ */
 int _strlen(char *s)
 {
 	int strlen = 0;
