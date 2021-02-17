4c5b883301aaf9c1e5951da20ea495d513d4c4af
darbumas <2314@holbertonschool.com>
2021-02-17 15:21:43 -0600

    add 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+int _strlen(char *);
+/**
+ * puts_half - ƒ() prints half of a string,
+ * followed by a new line.
+ * @str: pointer to a char (base index of string).
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+	int str_len = _strlen(str);
+	int mark;
+
+	if ((str_len % 2) == 0)
+		mark = str_len / 2;
+	else
+		mark = str_len - ((str_len - 1) / 2);
+	int i = mark;
+
+	while (i < str_len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar(10);
+}
+
+/**
+ * _strlen - ƒ() returns the length of a string.
+ * @s: pointer to a char (the string in question).
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int strlen = 0;
+
+	while (s[strlen] != '\0')
+		strlen++;
+	return (strlen);
+}
