66a7b5815b7b8ec5e21c9c64f5730fbc69c8dbf6
darbumas <2314@holbertonschool.com>
2021-02-18 12:41:07 -0600

    initial commit, mostly empty files

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Learning Objectives:
+
+>> POINTERS - ARRAYS - STRINGS
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,16 @@
+#ifndef _HOLBERTON
+#define _HOLBERTON
+
+#include <unistd.h>
+
+int _putchar(char);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
+void reverse_array(int *, int);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *rot13(char *);
+
+#endif /* holberton.h */
