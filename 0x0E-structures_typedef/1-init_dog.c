cf2c94ed25b5ada1b832f9f2872ada20405c22b8
darbumas <2314@holbertonschool.com>
2021-03-08 17:59:56 -0600

    add 1, add <stddef.h> to dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - ƒ() initializes a var of type "struct dog"
+ * @d: pointer to struct; var to be initialized.
+ * @name: pointer to char; name of new dog.
+ * @age: float type, doggie's age.
+ * @owner: pointer to char; doggies' owner.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->owner = owner;
+		d->name = name;
+		d->age = age;
+	}
+}
