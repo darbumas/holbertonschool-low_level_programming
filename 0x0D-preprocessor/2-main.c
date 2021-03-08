53260c0790b7e1c0eb0265a1e1a097528492ab50
darbumas <2314@holbertonschool.com>
2021-03-08 15:52:26 -0600

    add 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints source code file name.
+ *
+ * Return: EXIT_SUCCESS
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
