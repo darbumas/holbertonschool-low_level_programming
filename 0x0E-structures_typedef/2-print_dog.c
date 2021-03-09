2945fcc4057a45590ab8a633da891511368d22c3
darbumas <2314@holbertonschool.com>
2021-03-08 18:46:52 -0600

    change pointer cast

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,7 +6,6 @@
  * print_dog - ƒ() prints a structure "struct dog"
  * @d: pointer to a struct.
  *
- * Return: void.
  */
 
 void print_dog(struct dog *d)
@@ -16,11 +15,11 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (%p)\n", d->name);
+			printf("Name: (nil)\n");
 		printf("Age: %lf\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (%p)\n", d->owner);
+			printf("Owner: (nil)\n");
 	}
 }
