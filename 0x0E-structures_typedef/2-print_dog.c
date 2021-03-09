fc8a772b61c11d8aa36d100afa3c60644ee56aa9
darbumas <2314@holbertonschool.com>
2021-03-09 14:31:03 -0600

    change (nil) to format

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,11 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)\n");
+			printf("Name: (%p)\n", d->name);
 		printf("Age: %.6f\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)\n");
+			printf("Owner: (%p)\n", d->owner);
 	}
 }
