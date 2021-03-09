7ac9ee26291e50942ccb8bf224b26a66f625267f
darbumas <2314@holbertonschool.com>
2021-03-08 18:21:55 -0600

    add 2; add stdio.h to dog.h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * print_dog - ƒ() prints a structure "struct dog"
+ * @d: pointer to a struct.
+ *
+ * Return: void.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %lf\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
