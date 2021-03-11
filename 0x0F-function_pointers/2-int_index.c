c334a27058cd6f9d2745ba7ac958affe71975144
darbumas <2314@holbertonschool.com>
2021-03-10 22:30:37 -0600

    add 2, move stdio to header file

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - ƒ() returns int; searches for an integer;
+ * the ƒ() takes a pointer to a normal array, size of array and a ƒ() pointer
+ * returning an int as parameter.
+ * @array: elements for the call-back ƒ().
+ * @size: size of the array.
+ * @cmp: pointer to a ƒ() to compare values.
+ * Return: int; index of the first element for which the cmpƒ() succeeds, -1
+ * otherwise
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	int i = 0;
+
+	while (size > 0 && i < size && array != NULL)
+	{
+		if (cmp(array[i++]))
+			return (i - 1);
+	}
+	if (*(array + i) == '\0')
+		return (-1);
+	return (-1);
+}
