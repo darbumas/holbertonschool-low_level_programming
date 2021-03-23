69cc44f463e898260bd4f23a083d8033c47fb3fa
darbumas <2314@holbertonschool.com>
2021-03-23 16:10:15 -0500

    fix loop

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,6 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		ref = ref->next;
 		if (ref == NULL)
 			return (fail);
+		i++;
 	}
 	latest = ref->next;
 	ref->next = latest->next;
