fefdac713a9041ba6c5adc040344135f0a549542
darbumas <2314@holbertonschool.com>
2021-03-23 13:02:14 -0500

    add 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - ƒ() returns size of a linked list; prints all members of the
+ * linked list.
+ * @h: read-only pointer to the head node.
+ * Return: count of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
