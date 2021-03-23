1b40fe07093a6840419334be3fe959f0864d8e5d
darbumas <2314@holbertonschool.com>
2021-03-23 13:26:34 -0500

    add 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - ƒ() returns a pointer to a new node;
+ * it adds a new node at the beginning of the linked list.
+ * @head: address of the pointer to the first node.
+ * @n: read-only data to store in the new node.
+ * Return: address of the new node, otherwise NULL.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *locale;
+
+	if (head == NULL)
+		return (NULL);
+	locale = malloc(sizeof(listint_t));
+
+	if (locale == NULL)
+		return (NULL);
+	locale->n = n;
+	locale->next = *head;
+	*head = locale;
+
+	return (locale);
+}
