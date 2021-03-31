c7de087341fc32f4f35109e87ad839f342ab1361
darbumas <2314@holbertonschool.com>
2021-03-31 10:55:13 -0500

    initial, add 0

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,10 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdio.h>
+#include <unistd.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void recursive_puts(unsigned long int num);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
