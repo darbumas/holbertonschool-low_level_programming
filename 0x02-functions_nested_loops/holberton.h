e455f17658fb21b2773254ef19fa5e93ec212970
darbumas <2314@holbertonschool.com>
2021-04-01 12:29:54 -0500

    update header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON
-#define _HOLBERTON
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
 
 int _putchar(char);
 int print_alphabet(void);
@@ -12,6 +12,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_times_table(int);
 
 #include <stdio.h>
 void print_to_98(int n);
