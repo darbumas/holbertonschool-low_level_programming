40fe2763a0579044140d4d4c2c329ccbfa1aeb09
darbumas <2314@holbertonschool.com>
2021-11-18 02:19:30 +0000

    add 1 and delete unecessary fls

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - a ƒ() that creates a file
+ * @filename: pointer to the file to be created
+ * @text_content: null terminated pointer to string to write to file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fd_file, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (*text_content == '\0')
+		return (1);
+
+	i = 0;
+	while (text_content[i] != '\0')
+		i++;
+
+	fd_file = write(fd, text_content, i);
+	if (fd_file == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
