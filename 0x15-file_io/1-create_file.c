966b84fe56a9cc515a09bce1636b4fa50886e40c
darbumas <2314@holbertonschool.com>
2021-11-18 02:27:15 +0000

    initialize indx earlier

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, fd_file, i;
+	int fd, fd_file, i = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,7 +21,6 @@ int create_file(const char *filename, char *text_content)
 	if (*text_content == '\0')
 		return (1);
 
-	i = 0;
 	while (text_content[i] != '\0')
 		i++;
 
