67d7b931f09f5469ddeeb82a916e102701b5bf91
darbumas <2314@holbertonschool.com>
2021-12-09 04:36:37 +0000

    solution #9

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_dlistint_backward - ƒ() prints elements of list in reverse.
+ * @h: const pointer to the first node of list.
+ * Return: the number of nodes in the list.
+ */
+
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t nodes = 0;
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->prev;
+		nodes++;
+	}
+	return (nodes);
+}
