9e010bf5baf7d438f9b1d6239e6a0fa791d8665e
darbumas <2314@holbertonschool.com>
2021-12-13 05:35:35 +0000

    refactor

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,22 +8,26 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned int i = 0;
 	hash_table_t *table;
+	hash_node_t **node;
 
-	if (!size)
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
-
-	table = calloc(1, sizeof(hash_table_t));
-	if (!table)
-		return (NULL);
-
-	table->size = size;
-
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (!table->array)
+	node = malloc(sizeof(hash_node_t *) * size);
+	if (node == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
+	while (i < size)
+	{
+		node[i] = NULL;
+		i++;
+	}
+
+	table->size = size;
+	table->array = node;
 	return (table);
 }
