2d7bae6a188f9563d176fb3f58d532e707cdbf15
darbumas <2314@holbertonschool.com>
2022-10-28 13:59:45 -0500

    linear jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -29,4 +29,22 @@ typedef struct skiplist_s
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
 #endif
