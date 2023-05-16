2fdb75caa3c2c95746c04a7ef2e88747338a69e9
darbumas <2314@holbertonschool.com>
2023-05-16 19:53:38 +0000

    changing to [%lu]

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -26,7 +26,8 @@ int interpolation_search(int *array, size_t size, int value)
 		pos = low + (((double)(high - low) / (array[high] - array[low]))
 				* (value - array[low]));
 
-		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%lu] = [%d]\n", (unsigned long)pos,
+				array[pos]);
 
 		if (array[pos] < value)
 			low = pos + 1;
